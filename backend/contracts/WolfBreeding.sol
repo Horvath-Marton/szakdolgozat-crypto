b1a9c26a62154775e82b1e79e96337e2f76dbbe3
Horváth Márton <horvathmarton02@gmail.com>
2021-12-24 16:20:15 +0100

    Added breedWolf function to WolfBreeding.sol
    Only placeholder, not final version

--- backend/contracts/WolfBreeding.sol
+++ backend/contracts/WolfBreeding.sol
@@ -3,5 +3,12 @@ pragma solidity >=0.8.10 <0.9.0;
 import "./WolfFactory.sol";
 
 contract WolfBreeding is WolfFactory {
-
+    
+    function breedWolves(uint _wolfId, uint _partnerDna) public {
+    require(msg.sender == wolfToOwner[_wolfId]);
+    Wolf storage myWolf = wolves[_wolfId];
+    _partnerDna = _partnerDna % dnaModulus;
+    uint newDna = (myWolf.dna + _partnerDna) / 2;
+    _createWolf("NewWolf", newDna);
+  }
 }
