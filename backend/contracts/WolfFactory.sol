2ba5d6fe60ab75236692e055590b7f461dd55550
Horváth Márton <horvathmarton02@gmail.com>
2021-12-24 11:28:23 +0100

    Added WolfFactory.sol at /backend/contracts

---
+++ backend/contracts/WolfFactory.sol
@@ -0,0 +1,43 @@
+pragma solidity >=0.8.10 <0.9.0;
+
+contract WolfFactory {
+
+    event NewWolf(uint wolfId, string name, uint dna);
+
+    uint dnaDigits = 32; 
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Wolf {
+        string name;
+        uint dna;
+        //uint cdtime;
+        //bool isAdult;
+        //bool isBreedable;
+        //uint price;    
+    }
+
+    Wolf[] public wolves;
+
+    mapping (uint => address) public wolfToOwner;
+    mapping (address => uint) ownerWolfCount;
+
+    function _createWolf (string memory _name, uint _dna) private {
+        wolves.push(Wolf(_name, _dna));
+        uint id = wolves.length - 1;
+        wolfToOwner[id] = msg.sender;
+        ownerWolfCount[msg.sender]++;
+        emit NewWolf(id, _name, _dna);
+    }
+    
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomWolf(string memory _name) public {
+        require(ownerWolfCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createWolf(_name, randDna);
+    }
+
+}
