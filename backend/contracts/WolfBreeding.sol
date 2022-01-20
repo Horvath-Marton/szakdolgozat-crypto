c832933b093991de715e6dce971f1bd3ad851774
Horváth Márton <horvathmarton02@gmail.com>
2022-01-20 23:29:36 +0100

    Added a lot thing

--- backend/contracts/WolfBreeding.sol
+++ backend/contracts/WolfBreeding.sol
@@ -1,14 +1,27 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.10 <0.9.0;
 
 import "./WolfFactory.sol";
 
 contract WolfBreeding is WolfFactory {
     
-    function breedWolves(uint _wolfId, uint _partnerDna) public {
+  function _triggerCooldown(Wolf storage _wolf) internal {
+    _wolf.cdtime = uint32(block.timestamp + cooldownTime);
+  }
+
+  function _isReady(Wolf storage _wolf) internal view returns (bool) {
+      return (_wolf.cdtime <= block.timestamp);
+  }
+
+  function breedWolves(uint _wolfId, uint _partnerDna) internal  {
     require(msg.sender == wolfToOwner[_wolfId]);
     Wolf storage myWolf = wolves[_wolfId];
+    require(_isReady(myWolf));
     _partnerDna = _partnerDna % dnaModulus;
     uint newDna = (myWolf.dna + _partnerDna) / 2;
     _createWolf("NewWolf", newDna);
+    _triggerCooldown(myWolf);
   }
+
+
 }
--- backend/contracts/WolfFactory.sol
+++ backend/contracts/WolfFactory.sol
@@ -1,18 +1,23 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.10 <0.9.0;
 
-contract WolfFactory {
+
+import "./ownable.sol";
+
+contract WolfFactory is Ownable {
 
     event NewWolf(uint wolfId, string name, uint dna);
 
     uint dnaDigits = 32; 
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Wolf {
         string name;
         uint dna;
-        //uint cdtime;
-        //bool isAdult;
-        //bool isBreedable;
+        uint cdtime;
+        bool isAdult;
+        bool isBreedable;
         //uint price;    
     }
 
@@ -22,7 +27,7 @@ contract WolfFactory {
     mapping (address => uint) ownerWolfCount;
 
     function _createWolf (string memory _name, uint _dna) internal {
-        wolves.push(Wolf(_name, _dna));
+        wolves.push(Wolf(_name, _dna, uint32(block.timestamp + cooldownTime), false, false));
         uint id = wolves.length - 1;
         wolfToOwner[id] = msg.sender;
         ownerWolfCount[msg.sender]++;
